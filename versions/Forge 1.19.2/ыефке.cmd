80655646b8d942b9232617c3ebee82e15722f749
max <max34.95@mail.ru>
2023-07-12 15:33:22 +0300

    обновления расположения фалов

---
+++ versions/Forge 1.19.2/ыефке.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\max\Desktop\new folder\EGOR_PIDR\versions\Forge 1.19.2
+java -jar Forge 1.19.2.jar --version 1.19.2 --gameDir C:\Users\max\Desktop\new folder\EGOR_PIDR --mods C:\Users\max\Desktop\new folder\EGOR_PIDR\mods
+ pause
